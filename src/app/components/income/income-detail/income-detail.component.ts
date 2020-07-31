cdd607dc7dede2d90453cd8944a6a861f60d3fa3
Robert Constantinescu <robertsci@github.com>
2020-07-31 07:16:51 +0300

    income-detail.component.html, changed table details and created income model

--- src/app/components/income/income-detail/income-detail.component.ts
+++ src/app/components/income/income-detail/income-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {Income} from "../../../shared/income.model";
 
 @Component({
   selector: 'app-income-detail',
@@ -14,34 +14,34 @@ export class IncomeDetailComponent implements OnInit {
   }
 
   editField: string;
-  personList: Array<any> = [
-    { id: 1, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 2, name: 'Guerra Cortez', age: 45, companyName: 'Insectus', country: 'USA', city: 'San Francisco' },
-    { id: 3, name: 'Guadalupe House', age: 26, companyName: 'Isotronic', country: 'Germany', city: 'Frankfurt am Main' },
-    { id: 4, name: 'Aurelia Vega', age: 30, companyName: 'Deepends', country: 'Spain', city: 'Madrid' },
-    { id: 5, name: 'Elisa Gallagher', age: 31, companyName: 'Portica', country: 'United Kingdom', city: 'London' },
+  incomeList: Income[] = [
+    { id: 1, type: 'SALARY', currentAmount: 30, goal: 45, recurrence: 'MONTHLY', yearlyAmount: 0 },
+    { id: 2, type: 'SALARY', currentAmount: 45, goal: 45, recurrence: 'MONTHLY', yearlyAmount: 0 },
+    { id: 3, type: 'DIVIDENDS', currentAmount: 26, goal: 61, recurrence: 'YEARLY', yearlyAmount: 0 },
+    { id: 4, type: 'RENT', currentAmount: 30, goal: 57, recurrence: 'YEARLY', yearlyAmount: 0 },
+    { id: 5, type: 'INTEREST', currentAmount: 31, goal: 48, recurrence: 'QUARTERLY', yearlyAmount: 0 },
   ];
 
-  awaitingPersonList: Array<any> = [
-    { id: 6, name: 'George Vega', age: 28, companyName: 'Classical', country: 'Russia', city: 'Moscow' },
-    { id: 7, name: 'Mike Low', age: 22, companyName: 'Lou', country: 'USA', city: 'Los Angeles' },
-    { id: 8, name: 'John Derp', age: 36, companyName: 'Derping', country: 'USA', city: 'Chicago' },
-    { id: 9, name: 'Anastasia John', age: 21, companyName: 'Ajo', country: 'Brazil', city: 'Rio' },
-    { id: 10, name: 'John Maklowicz', age: 36, companyName: 'Mako', country: 'Poland', city: 'Bialystok' },
+  awaitingPersonList: Income[] = [
+    { id: 1, type: 'SALARY', currentAmount: 30, goal: 45, recurrence: 'Spain', yearlyAmount: 0 },
+    { id: 2, type: 'SALARY', currentAmount: 45, goal: 45, recurrence: 'USA', yearlyAmount: 0 },
+    { id: 3, type: 'DIVIDENDS', currentAmount: 26, goal: 61, recurrence: 'Germany', yearlyAmount: 0 },
+    { id: 4, type: 'RENT', currentAmount: 30, goal: 57, recurrence: 'Spain', yearlyAmount: 0 },
+    { id: 5, type: 'INTEREST', currentAmount: 31, goal: 48, recurrence: 'United Kingdom', yearlyAmount: 0 }
   ];
 
   updateList(id: number, property: string, event: any) {
     const editField = event.target.textContent;
-    this.personList[id][property] = editField;
+    this.incomeList[id][property] = editField;
   }
 
   remove(id: any) {
-    this.awaitingPersonList.push(this.personList[id]);
-    this.personList.splice(id, 1);
+    this.awaitingPersonList.push(this.incomeList[id]);
+    this.incomeList.splice(id, 1);
   }
 
   add() {
-    this.personList.push({ id: 10, name: '', age: 0, companyName: '', country: '', city: '' });
+    this.incomeList.push({ id: 0, type: '', currentAmount: 0, goal: 0, recurrence: '', yearlyAmount: 0 });
   }
 
   changeValue(id: number, property: string, event: any) {
---
+++ src/app/shared/income.model.ts
@@ -0,0 +1,14 @@
+export class Income {
+
+
+  public type: string;
+  public currentAmount: number;
+  public yearlyAmount: number;
+  public recurrence: string;
+  public goal: number;
+  public id?: number;
+  public username?: string;
+
+  constructor(){}
+
+}
